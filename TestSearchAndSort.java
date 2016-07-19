46b083a284d95cb8fdd7288a49b83ddf25c44230
Pratap Singh Ranawat <pratap.ranawat@metacube.com>
2016-07-19 13:27:40 +0530

    Add files via upload

--- TestSearchAndSort.java
+++ TestSearchAndSort.java
@@ -5,6 +5,8 @@
 
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,27 +14,42 @@ import org.junit.Test;
 public class TestSearchAndSort {
 
 	SearchAndSort sas;
-	int[] arr,arr1;
+	int[] inputArr,expectedOutputArr;
+	
+	Scanner sc;
 	@Before
 	public void setUp() throws Exception {
 		sas = new SearchAndSort();
+		//arr= new int[]{1,2,3,4,8,6,7,5,9};
+	//	arr1= new int[]{1,2,3,4,5,6,7,8,9};
 		
-		arr= new int[]{1,2,3,4,8,6,7,5,9};
-		arr1= new int[]{1,2,3,4,5,6,7,8,9};
+		sc = new Scanner(System.in);
+		int arrSize = sc.nextInt();
+		
+		inputArr = new int[arrSize];
+		expectedOutputArr = new int[arrSize];
+		 
+		for(int i=0;i<arrSize;i++){
+			inputArr[i] = sc.nextInt();
+		}
+		
+		for(int i=0;i<arrSize;i++){
+			expectedOutputArr[i] = sc.nextInt();
+		}
 	}
 
 	@Test
 	public void test() {
 		
 		//tesing linear search
-		assertEquals(8, sas.linearSearch(arr, 8, 0)); 
+		assertEquals(8, sas.linearSearch(inputArr, 8, 0)); 
 		
 		//testing binary search
-		assertEquals(8, sas.binarySearch(arr, 8, 0,arr.length));
+		assertEquals(8, sas.binarySearch(inputArr, 8, 0,inputArr.length));
 		
 		// testing QuickSort
-		sas.quickSort(arr, 0, arr.length-1);
-		assertArrayEquals(arr1, arr);
+		sas.quickSort(inputArr, 0, inputArr.length-1);
+		assertArrayEquals(expectedOutputArr, inputArr);
 	}
 
 }
