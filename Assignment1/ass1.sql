d01192431c2fa09ef743886221c0c06aa6e369c4
Pratap Singh Ranawat <pratap.ranawat@metacube.com>
2016-08-15 21:51:34 +0530

    Update ass1.sql

--- Assignment1/ass1.sql
+++ Assignment1/ass1.sql
@@ -1,3 +1,4 @@
+/** assignment 1 */
 USE lis;
 
 /**  Question 1 */
@@ -28,13 +29,3 @@ SELECT m.member_nm FROM members m JOIN members k ON m.categroy = k.categroy WHER
 SELECT i.member_id as 'issued by', i.issue_dt as 'issued date', i.accession_no as 'book number',r.return_dt as 'return on'  
 FROM book_issue i LEFT JOIN book_return r 
 ON i.issue_dt = r.issue_dt AND i.accession_no = r.accession_no AND i.member_id = r.member_id ORDER BY i.member_id;
-
-
-/**
-assignment 2 rewritten Query for Question 9 order by with member_nm
-*/
-SELECT i.member_id as 'issued by',m.member_nm as 'name', i.issue_dt as 'issued date', i.accession_no as 'book number',r.return_dt as 'return on'  
-FROM book_issue i LEFT JOIN book_return r  
-ON i.issue_dt = r.issue_dt AND i.accession_no = r.accession_no AND i.member_id = r.member_id 
-JOIN members m ON i.member_id = m.member_id ORDER BY i.member_id,m.member_nm;
-
