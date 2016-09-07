fd754b35516981b8d4ae57ad7ef3964b4c846a2a
Pratap Singh Ranawat <pratap.ranawat@metacube.com>
2016-09-07 19:12:06 +0530

    Add files via upload

---
+++ Assignment-3/javaScript.js
@@ -0,0 +1,56 @@
+$(document).ready(init);
+
+function init() {
+    $("span.message").click(Q1);
+	$("div.box:first").click(Q2);
+	$("button").click(Q3);
+    $('img[alt="hello"]').click(Q4);
+    //$('#myDiv input[type="text"] input[name^='txt']').click(fnHide);
+    $('p:not(.box)').click(Q6);
+    $('div.box, div.error').click(Q7);
+    $('div.box div.error').click(Q8);
+    $('div#myDiv span.info').click(Q9);
+}	
+
+function Q1(){
+  
+  $(this).css("color","red");
+
+}
+
+function Q2(){
+  
+  $(this).css("background-color","gray");
+
+}
+
+function Q3(){
+  
+  $(this).css("color","red");
+
+}
+
+function Q4(){
+  
+  $(this).css("height","100px");
+
+}
+
+function Q6(){
+  
+  $(this).css("color","gray");
+
+}
+
+function Q7(){
+  
+  $(this).css("background-color","black");
+
+}
+
+function Q8(){
+  
+  $(this).css("background-color","yellow");
+
+}
+
