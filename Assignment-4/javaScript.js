9d2dd2e38564d2e7a3c99f8b3482136316f4257d
Pratap Singh Ranawat <pratap.ranawat@metacube.com>
2016-09-08 13:54:35 +0530

    alternateRowColor

---
+++ Assignment-4/javaScript.js
@@ -0,0 +1,9 @@
+$(document).ready(init);
+
+function init() {
+    
+    $('tr:even').css("background-color", "black").css("color", "white");
+    $('tr:odd').css("background-color", "white").css("color", "black");
+}
+
+
