419ec686833aa15d9eb706e2bceb2c760b63714d
Pratap Singh Ranawat <pratap.ranawat@metacube.com>
2016-09-16 12:06:06 +0530

    working...

--- Assignment-2/javaScript.js
+++ Assignment-2/javaScript.js
@@ -4,12 +4,8 @@ $(document).ready(init);
 function init() {
     console.log("Hello");
     
-   $.getJSON("jQuery2/employee.json", displayName);
-	$.getJSON("jQuery2/employee.json", function(data){
+   $.getJSON("employee.json", displayName);
 	
-	$("#optionList").onchange(displayDetails);	
-	
-	});
 	
 	
     
@@ -19,11 +15,20 @@ function displayName(data){
        
        var list="";
     	data1 = data;
-    console.log(data.employee.length+"");
+   
         for(i=0;i<data.employee.length;i++){
             console.log(data.employee[i].name+"")
            list=list + "<option value=" + i + ">" + data.employee[i].name + "</option>";}
         document.getElementById("optionList").innerHTML = list;
+		
+		$("#optionList").change(function(data){
+			
+			alert("Ok");
+			var index = Number($(this).val());
+			alert(index);
+			document.getElementById("name").innerHTML = data.employee[index].name;
+			
+		});
    }
 
 function displayDetails(data) {
