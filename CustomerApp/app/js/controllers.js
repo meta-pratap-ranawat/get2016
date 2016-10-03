0933406b245bbec7bdf9fc485bcb2338e1db0438
Pratap Singh Ranawat <pratap.ranawat@metacube.com>
2016-10-03 11:35:00 +0530

    Add files via upload

--- CustomerApp/app/js/controllers.js
+++ CustomerApp/app/js/controllers.js
@@ -1,10 +1,35 @@
-var myJava = angular.module('myJava', []);
+var myJava = angular.module('myJava', ["ngRoute"]);
+
+myJava.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'view/gridView.html',
+        controller: 'listView'
+      }).
+      when('/home', {
+        templateUrl: 'view/gridView.html',
+        controller: 'listView'
+      }).
+      when('/menu1', {
+        templateUrl: 'view/listView.html',
+        controller: 'listView'
+      }).
+      when('/menu2', {
+        templateUrl: 'view/newAddView.html',
+        controller: 'listView'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
+
 
 myJava.controller('listView', listView);
 
-listView.$inject = ['$scope','$http'];
+listView.$inject = ['$scope','$http','$location'];
 
-function listView($scope, $http){
+function listView($scope, $http, $location){
  var url = "http://localhost:8080/CustomerRestFulService/rest/CustomerService/customers";
 
  var onSuccess = function (data, status, headers, config) {
@@ -17,4 +42,14 @@ function listView($scope, $http){
              }
 
   var promise = $http.get(url).success(onSuccess).error(onError);
+
+  $scope.submitForm = function(){
+      $scope.customers.push($scope.newCustomer);
+    //  $location.path('#home');
+  };
+
+  $scope.deleteRecord = function(customer){
+    var index = $scope.customers.indexOf(customer);
+  $scope.customers.splice(index, 1);
+  };
 }
