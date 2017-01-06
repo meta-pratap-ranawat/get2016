d440ee2daf547002ebffb354c2053013fb5c3a02
Pratap Singh Ranawat <pratap.ranawat@metacube.com>
2017-01-06 10:26:47 +0530

    Add files via upload

---
+++ CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClientCertTests.Models;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Http.Headers;
+
+namespace ClientCertTests.Controllers
+{
+    public class CustomersController : ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            X509Certificate2 clientCertInRequest = RequestContext.ClientCertificate;
+            //X509Certificate2 clientCertInRequest = Request.GetClientCertificate();
+            if (clientCertInRequest != null)
+            {
+                IList<Customer> customers = new List<Customer>();
+                customers.Add(new Customer() { Name = "Nice customer", Address = "USA", Telephone = "123345456" });
+                customers.Add(new Customer() { Name = "Good customer", Address = "UK", Telephone = "9878757654" });
+                customers.Add(new Customer() { Name = "Awesome customer", Address = "France", Telephone = "34546456" });
+                return Ok<IList<Customer>>(customers);
+            }
+            AuthenticationHeaderValue authHeaderValue = new AuthenticationHeaderValue("ClientCert");
+            return Unauthorized(authHeaderValue);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCaller
+{
+    class Program
+    {
+        private static X509Certificate2 GetClientCertificate()
+        {
+            Console.WriteLine("debugtoCheck");
+            X509Store userCaStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            try
+            {
+                Console.WriteLine("debugtoCheck");
+                userCaStore.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection certificatesInStore = userCaStore.Certificates;
+                X509Certificate2Collection findResult = certificatesInStore.Find(X509FindType.FindBySubjectName, "localtestclientcert", true);
+                X509Certificate2 clientCertificate = null;
+                if (findResult.Count == 1)
+                {
+                    clientCertificate = findResult[0]; Console.WriteLine("got client");
+                }
+                else
+                {
+                    throw new Exception("Unable to locate the correct client certificate.");
+                }
+                return clientCertificate;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                userCaStore.Close();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //HttpClient client = new HttpClient()
+            //{
+            //    BaseAddress = new Uri("http://localhost:56949/")
+            //};
+
+            //HttpResponseMessage response = client.GetAsync("customers").Result;
+            //string responseContent = response.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine(responseContent);
+            //Console.ReadKey();
+
+            try
+            {
+                Console.WriteLine("debugtoCheck");
+                X509Certificate2 clientCert = GetClientCertificate();
+                Console.WriteLine("debug one"+ clientCert==null);
+                WebRequestHandler requestHandler = new WebRequestHandler();
+                requestHandler.ClientCertificates.Add(clientCert);
+
+                HttpClient client = new HttpClient(requestHandler)
+                {
+                    BaseAddress = new Uri("http://localhost:3020/")
+                };
+
+                HttpResponseMessage response = client.GetAsync("customers").Result;
+                response.EnsureSuccessStatusCode();
+                string responseContent = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception while executing the test helllo code: {0}", ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
